c55a07039b05aee1b796f05cc44438f0f3dce364
Prince200804 <princeadani01@gmail.com>
2026-02-25 16:42:58 +0530

    Fix: remove Clerk from Edge middleware, move auth to server component layout

--- middleware.js
+++ middleware.js
@@ -1,18 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
-  if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
-    return redirectToSignIn();
-  }
-
+export default function middleware() {
   return NextResponse.next();
-});
+}
 export const runtime = 'nodejs';
 export const config = {
   matcher: [
